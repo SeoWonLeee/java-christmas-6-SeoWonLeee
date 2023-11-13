59885075098c788f149fb5ee6e73b12c34beddfe
SeoWonLeee <scw.dltjdnjs@gmail.com>
2023-11-13 11:52:06 +0900

    feat: 어플리케이션

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,8 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView outputView = new OutputView();
+        Controller controller = new Controller(outputView);
+        controller.run();
     }
 }
