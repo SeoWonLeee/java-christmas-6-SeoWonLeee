57ca42685baee4fbc273bdd555c72af185f6d736
SeoWonLeee <scw.dltjdnjs@gmail.com>
2023-11-13 11:44:10 +0900

    feat: 주문받은 메뉴 관리

---
+++ src/main/java/christmas/MenuOrder.java
@@ -0,0 +1,36 @@
+package christmas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuOrder {
+    private final Map<String, Integer> orderDetails;
+
+    private final Validator validator;
+
+    public MenuOrder(Menu menu) {
+        this.orderDetails = new HashMap<>();
+
+        this.validator = new Validator(menu);
+    }
+
+    public void addOrder(String menuItem, int quantity) {
+        try {
+            validator.validateQuantity(quantity);
+
+            validator.validateMenu(menuItem);
+
+            if (orderDetails.containsKey(menuItem)) {
+                orderDetails.put(menuItem, orderDetails.get(menuItem) + quantity);
+            } else {
+                orderDetails.put(menuItem, quantity);
+            }
+        } catch (IllegalArgumentException e) {
+            validator.throwInvalidMenuOrderException(e.getMessage());
+        }
+    }
+
+    public Map<String, Integer> getOrderDetails() {
+        return new HashMap<>(orderDetails);
+    }
+}
