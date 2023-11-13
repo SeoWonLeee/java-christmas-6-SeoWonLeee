2d2358e2ba09b177d30359be697996bfa7878f0d
SeoWonLeee <scw.dltjdnjs@gmail.com>
2023-11-13 23:24:24 +0900

    fix: 프로모션 날짜 지정 및 수정

--- src/main/java/christmas/Promotion.java
+++ src/main/java/christmas/Promotion.java
@@ -5,7 +5,6 @@ import java.time.temporal.ChronoUnit;
 
 public class Promotion {
     private static final LocalDate CHRISTMAS_START_DATE = LocalDate.of(2023, 12, 1);
-
     private static final LocalDate CHRISTMAS_END_DATE = LocalDate.of(2023, 12, 25);
 
     public int calculateChristmasDiscount() {
@@ -33,20 +32,25 @@ public class Promotion {
     public int calculateWeekendDiscount(int mainCount) {
         LocalDate currentDate = LocalDate.now();
 
-        if (currentDate.getDayOfWeek().getValue() >= 5 && currentDate.getDayOfWeek().getValue() <= 7) {
+        if (currentDate.getDayOfWeek().getValue() >= 5 && currentDate.getDayOfWeek().getValue() <= 6) {
             return mainCount * 2023;
         }
 
         return 0;
     }
 
-    public int calculateSpecialDiscount(boolean hasStar) {
-        if (hasStar) {
+    public int calculateSpecialDiscount(MenuOrder menuOrder) {
+        LocalDate currentDate = LocalDate.now();
+        int dayOfMonth = currentDate.getDayOfMonth();
+
+        if (currentDate.getMonthValue() == 12 && (dayOfMonth == 3 || dayOfMonth == 10 || dayOfMonth == 17
+                || dayOfMonth == 24 || dayOfMonth == 25 || dayOfMonth == 31)) {
             return 1000;
-        } else {
-            return 0;
         }
+
+        return 0;
     }
+
 
     public int calculateGiftDiscount(int totalOrderPrice) {
         if (totalOrderPrice >= 120000) {
